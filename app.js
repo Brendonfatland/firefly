4f8a34524338f68554d8d59f380ff7b78cb27642
Brendon Fatland <b.fatland@gmail.com>
2018-08-06 16:30:06 -0400

    when the make is changed it updates the options for the model

--- app.js
+++ app.js
@@ -33,7 +33,82 @@ $(document).ready(function(){
   });
 
   $(".displayMoreButton").click(function(){
-
     $(".displayMoreArea").toggle();
 });
+/* $('#make option').each(function() {
+
+    if (this.selected.val() == "dell") {
+       $("#modelDell").show();
+    }
+}); */
+
+
+/* function changeModel(selectedMake){
+
+} */
+
+$('#make').change(function(){
+    selection = $(this).val();
+    switch(selection)
+    {
+      case 'lenovo':
+       $('.model').hide();
+        $('#modelLenovo').show();
+        break;
+      case 'dell':
+      $('.model').hide();
+       $('#modelDell').show();
+        break;
+        case 'acer':
+        $('.model').hide();
+         $('#modelAcer').show();
+          break;
+          case 'google':
+          $('.model').hide();
+           $('#modelGoogle').show();
+            break;
+            case 'asus':
+            $('.model').hide();
+             $('#modelAsus').show();
+              break;
+
+    }
+  });
+
+var selectedMake = $('#make option').val();
+console.log(selectedMake);
+
+/*  $( "#make" ).change(function() {
+  alert( "Handler for .change() called." );
+});
+*/
+
+/*  $("#make").change(function(){
+        $(this).css("background-color", "#D6D6FF");
+    });
+
+  /* $('#make option').each(function() {
+    if($(this).is(':selected')){
+      $().show();
+    }
+
+    if ($( "#make" ).val().selected){
+      console.log(this);
+    } */
+
+
+  /*  $( "#make" ).change(function() {
+        var str = "";
+        $( "select option:selected" ).each(function() {
+          str += $( this ).text() + " ";
+        });
+        $( "div" ).text( str );
+      })
+      .trigger( "change" );
+      */
+
+      var el = document.querySelector("#make > option:nth-child(4)");
+      console.log(el);
+
+
 })
